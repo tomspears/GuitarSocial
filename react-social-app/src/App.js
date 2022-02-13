ae4d7800c5a19ef97a31c5d5238b34bf82236842
tomspears <tomspearsinc@gmail.com>
2022-02-13 02:56:10 -0500

    commit

--- react-social-app/src/App.js
+++ react-social-app/src/App.js
@@ -1,7 +1,4 @@
 import Home from "./pages/home/Home";
-import Profile from "./pages/profile/Profile"
-import Login from "./components/login/Login";
-import Register from "./components/register/Register";
 
 function App() {
   return <Home />
